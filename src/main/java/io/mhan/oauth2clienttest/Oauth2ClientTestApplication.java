aad806ae8d2616a2d7f585caa85b01f566588a8e
audgks5551 <audgsk5551@daum.net>
2023-05-01 20:01:30 +0900

    init

---
+++ src/main/java/io/mhan/oauth2clienttest/Oauth2ClientTestApplication.java
@@ -0,0 +1,13 @@
+package io.mhan.oauth2clienttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ClientTestApplication.class, args);
+    }
+
+}
